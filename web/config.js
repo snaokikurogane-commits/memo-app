d65e0d6ca5aafddc3101bf433d60e7a7284686e3
snaokikurogane-commits <s_naoki_kurogane@yahoo.co.jp>
2026-08-28 23:17:36 +0900

    Update Supabase anon key in config.js

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 window.PEOPLE_NOTEBOOK_CONFIG = {
   supabaseUrl: 'https://nnhtmjvoeupuaoizovac.supabase.co',
-  supabaseAnonKey: 'ここにコピーしたPublishable key',
+  supabaseAnonKey: 'sb_publishable_kkxRB0QvPtu8Eyxd-QyGcw_w38IUPdD',
   currentFiscalYear: '令和8年度'
 };
